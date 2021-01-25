93eed7dbcdb0b7ede4b90dae5ff2bbbb14ce29a1
Coaxial <thecoaxial@gmail.com>
2021-01-25 15:26:38 -0500

    Finished controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,3 +15,33 @@ router.get("/", function(req, res){
         res.render("index", burgObj);
     });
 });
+
+router.post("/api/burgers", function(req, res){
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result){
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/cats/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condtion", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result){
+        if (result.changedRows == 0){
+            // if nothing changed, return 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+module.exports = router;
