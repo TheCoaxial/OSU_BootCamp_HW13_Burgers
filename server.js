fb08e74fb7888fa4b52d11d511f9d0d8041c8c5b
Coaxial <thecoaxial@gmail.com>
2021-01-26 12:00:26 -0500

    Filled out server.js

--- server.js
+++ server.js
@@ -3,3 +3,28 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+var app = express();
+
+//Serve static content for the app from "public"
+app.use(express.static("public"));
+
+//Parse app body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Routes
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+//start server
+app.listen(PORT, function() {
+
+    console.log("Server listening on: http://localhost:" + PORT);
+});
